b894bb25dd67c68087ff41ba27227fbd00ba15ba
Mariel Baldonado <54623100+MarielleBaldonado@users.noreply.github.com>
2023-06-24 13:15:54 +0800

    Add project files.

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,39 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Future_Craft_Digital_02
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new Bundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new Bundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new Bundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new Bundle("~/bundles/javascript").Include(
+                      "~/Scripts/js/site.js",
+                      "~/Scripts/js/owl.carousel.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/css/styles.css",
+                      "~/Content/css/slick.css",
+                      "~/Content/css/flaticon.css",
+                      "~/Content/css/boxicons.min.css",
+                      "~/Content/css/owl.carousel.min.css"
+                      ));
+        }
+    }
+}
---
+++ Scripts/js/site.js
@@ -0,0 +1,72 @@
+﻿
+function toggleNavbar() {
+    var navbar = document.getElementById("navbarSupportedContent");
+    navbar.classList.toggle("show");
+    document.querySelector(".navbar-toggler").classList.toggle("collapsed");
+}
+
+function handleScroll() {
+    var header = document.getElementById('header');
+    if (window.scrollY > 170) {
+        header.classList.add('is-sticky');
+    } else {
+        header.classList.remove('is-sticky');
+    }
+}
+
+document.addEventListener('scroll', handleScroll);
+
+
+// Products Details Image Slides
+$(document).ready(function () {
+    var owl = $(".products-details-image-slides");
+    owl.owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        items: 1,
+        smartSpeed: 1000,
+        autoplayTimeout: 5000,
+        navText: [
+            "<i class='bx bx-chevron-left'></i>",
+            "<i class='bx bx-chevron-right'></i>"
+        ]
+    });
+});
+
+// Get references to the tab elements
+const tabs = document.querySelectorAll('#tabs li');
+
+// Get references to the tab content elements
+const tabContents = document.querySelectorAll('.content');
+
+// Add click event listeners to the tabs
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+        // Remove 'active' class from all tabs and add it to the clicked tab
+        tabs.forEach((tab) => tab.classList.remove('active'));
+        tab.classList.add('active');
+
+        // Hide all tab content and display the corresponding content for the clicked tab
+        tabContents.forEach((content) => content.classList.remove('show'));
+        tabContents[index].classList.add('show');
+    });
+});
+
+// Initialize the accordion
+document.addEventListener("DOMContentLoaded", function () {
+    const accordionItems = document.querySelectorAll("[data-accordion-component='AccordionItem']");
+
+    accordionItems.forEach(function (item) {
+        const button = item.querySelector(".accordion__button");
+        const panel = item.querySelector(".accordion__panel");
+
+        button.addEventListener("click", function () {
+            const expanded = button.getAttribute("aria-expanded") === "true";
+
+            button.setAttribute("aria-expanded", !expanded);
+            panel.hidden = expanded;
+        });
+    });
+});
